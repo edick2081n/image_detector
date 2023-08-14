7e1589193a09f37ca1b2ef2a77c8f895103d059f
Eduard <edick2081n@yahoo.com>
2023-08-14 16:17:17 +0300

    Secret key removed

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from .config import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9erp!rfizvt()^^!65tw4)!w#tui=9z^cne!s#5l)2p0=btg25'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
